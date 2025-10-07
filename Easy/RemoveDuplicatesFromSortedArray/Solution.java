59748a75a583486c75f8b98eca11df6e1faacb9b
Md Anees Khan <khanmdanis145@gmail.com>
2025-10-08 00:40:24 +0530

    Create Solution.java

---
+++ Easy/RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        int temp = nums[0]; 
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] != temp) {
+                nums[++k] = nums[i];
+                temp = nums[i];
+            }
+        }
+        return k + 1;
+    }
+}
