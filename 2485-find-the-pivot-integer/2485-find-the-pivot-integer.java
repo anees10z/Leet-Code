f2b592248fbab10688fc8fb0b0639a3510135a9d
Md Anees Khan <khanmdanis145@gmail.com>
2025-10-23 22:35:55 +0530

    Time: 2 ms (21.57%), Space: 41.4 MB (8.63%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,27 +1,23 @@
 class Solution {
     public int pivotInteger(int n) {
-        if(n==1) return 1;
-        int leftSum = 0, rightSum = 0;
-        // prefix sum in same array
-        int nums[] = new int[n+1];
-        for(int i = 0 ; i<=n;++i){
+        if (n == 1)
+            return 1;
+        // create an array and store 1 to n
+        int nums[] = new int[n + 1];
+        for (int i = 0; i <= n; ++i) {
             nums[i] = i;
         }
+        // prefix sum array
         for (int i = 2; i < nums.length; ++i) {
             nums[i] = nums[i] + nums[i - 1];
         }
+
+        int leftSum = 0, rightSum = 0;
         int end = nums.length - 1;
+
         for (int i = 2; i < nums.length; ++i) {
-            // if (i == 1) {
-            //     leftSum = num;
-            //     rightSum = nums[end] - nums[i];
-            // } else if (i == end) {
-            //     rightSum = 0;
-            //     leftSum = nums[end - 1];
-            // } else {
-                leftSum = nums[i];
-                rightSum = nums[end] - nums[i-1];
-            // }
+            leftSum = nums[i];
+            rightSum = nums[end] - nums[i - 1];
             if (leftSum == rightSum)
                 return i;
         }
