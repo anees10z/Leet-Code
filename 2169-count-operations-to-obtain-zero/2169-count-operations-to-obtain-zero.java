40fdd3a4a4a8a12f69cc79202d45e40e0cc5d25f
Md Anees Khan <khanmdanis145@gmail.com>
2025-11-09 12:56:33 +0530

    Time: 0 ms (100%), Space: 42.1 MB (18.76%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,15 +1,26 @@
 class Solution {
     public int countOperations(int num1, int num2) {
         int opCount = 0;
-        while(num1 != 0 && num2 != 0){
-            if(num1>=num2){
-                num1-=num2;
-                opCount++;
-            }else{
-                num2-=num1;
-                opCount++;
+        // optimized with divide + modulo-> O(log(min(num1, num2)))
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                opCount += num1 / num2;
+                num1 %= num2;
+            } else {
+                opCount += num2 / num1;
+                num2 %= num1;
             }
         }
         return opCount;
+        // Repeated subtraction -->  O(max(num1, num2))
+        /*while(num1 != 0 && num2 != 0){
+            if(num1>=num2){
+                num1-=num2;
+            }else{
+                num2-=num1;
+            }
+            opCount++;
+        }
+        return opCount; */
     }
 }
