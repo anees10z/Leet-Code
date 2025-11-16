3914ef521ef9272ac3fb80ac10a02e6815d70829
Md Anees Khan <khanmdanis145@gmail.com>
2025-11-16 11:04:18 +0530

    Time: 3 ms (58.51%), Space: 52.4 MB (7.71%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,13 +5,13 @@ class Solution {
             if(nums[i]==0) continue;
             nums[i]= nums[i]+nums[i-1];
         }
-        // Arrays.sort(nums);
+       
         int max = Integer.MIN_VALUE;
         for(int i = 0;i<nums.length;++i){
             if(nums[i]>max)
                 max=nums[i];
         }
-        // return nums[nums.length-1];
+        
         return max;
     }
 }
