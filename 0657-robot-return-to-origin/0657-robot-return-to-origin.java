9fee72a3f28684edb4a3972b13e75c927e057979
Md Anees Khan <khanmdanis145@gmail.com>
2026-04-05 16:26:14 +0530

    Time: 6 ms (49.49%), Space: 45.3 MB (91.72%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); ++i) {
+            char curr = moves.charAt(i);
+            switch (curr) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+        if (x == 0 && y == 0)
+            return true;
+        else
+            return false;
+    }
+}
