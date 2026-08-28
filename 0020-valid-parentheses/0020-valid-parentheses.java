5b7d1ca9af27ab6c37bb4bbd81c5e60c599a77bb
Md Anees Khan <khanmdanis145@gmail.com>
2026-08-28 09:36:31 +0530

    Time: 3 ms (86.07%), Space: 43.3 MB (57.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,14 +2,14 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        Deque<Character> st = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); ++i) {
             char curr = s.charAt(i);
             if (curr == '(' || curr == '{' || curr == '[') {
                 st.push(curr);
             } else {
-                if (st.empty()) {
+                if (st.isEmpty()) {
                     return false;
                 }
                 if ((curr == ')' && st.peek() == '(') || (curr == '}' && st.peek() == '{')
@@ -21,7 +21,7 @@ class Solution {
             }
 
         }
-        if (!st.empty())
+        if (!st.isEmpty())
             return false;
 
         return true;
