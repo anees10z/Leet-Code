598976912aa0df642ef8d09acb683bc10892a4d4
Md Anees Khan <khanmdanis145@gmail.com>
2026-09-08 13:49:20 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        // int len = (int) Math.log10(n)+1;
-
-        if(n<1000) return 0;
+        int len = (int) Math.log10(n)+1;
+        
+        if(len<4) return 0;
 
         return n-999;
     }
