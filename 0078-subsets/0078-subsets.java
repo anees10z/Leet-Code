1671b4aa056c831a62ef8cc1715a8e6a2e06308f
Md Anees Khan <khanmdanis145@gmail.com>
2026-09-10 16:42:33 +0530

    Time: 1 ms (86.97%), Space: 44.2 MB (56.55%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void find(int[] nums, List<List<Integer>> res, int idx, List<Integer> curr) {
+        if (idx == nums.length) {
+            res.add(new ArrayList(curr));
+            return;
+        }
+        curr.add(nums[idx]);
+        find(nums, res, idx + 1, curr);
+        curr.remove(curr.size()-1);
+        find(nums, res, idx+1, curr);
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        find(nums, res, 0, curr);
+        return res;
+    }
+}
