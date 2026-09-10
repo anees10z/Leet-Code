02495134de499ace72680ed8e3f87845b9c6f808
Md Anees Khan <khanmdanis145@gmail.com>
2026-09-10 16:45:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (38.3%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -5,16 +5,15 @@ class Solution {
             return;
         }
         curr.add(nums[idx]);
-        find(nums, res, idx + 1, curr);
-        curr.remove(curr.size() - 1);
-        find(nums, res, idx + 1, curr);
+        find(nums, res, idx + 1, curr); // case add in set
+        curr.remove(curr.size() - 1); // remove current added element from list
+        find(nums, res, idx + 1, curr); //backtrack in case of not add in set
 
     }
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-
         find(nums, res, 0, curr);
         return res;
     }
