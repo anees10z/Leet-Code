c0863e77da12fd42c7426a48c6ee4a42bf4d227f
Md Anees Khan <khanmdanis145@gmail.com>
2026-09-26 12:39:42 +0530

    Time: 32 ms (90.95%), Space: 90.8 MB (71.04%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,31 +1,25 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         Map<String, String> keys = new HashMap<>();
-        StringBuilder res = new StringBuilder("");
+        StringBuilder res = new StringBuilder();
+
         int len = s.length();
-        int sIdx = -1;
-        int eIdx = -1;
-        for (int i = 0; i < knowledge.size(); ++i) {
-            String key = knowledge.get(i).get(0);
-            String value = knowledge.get(i).get(1);
-            keys.put(key, value);
+
+        for (int i = 0; i < knowledge.size(); i++) {
+            keys.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
         }
 
-        for (int i = 0; i < len; ++i) {
+        for (int i = 0; i < len; i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
-                sIdx = i;
-                while (ch != ')') {
+                int startIdx = i;
+                while (s.charAt(i) != ')') {
                     ++i;
-                    ch = s.charAt(i);
                 }
-                eIdx = i;
-                String key = s.substring(sIdx + 1, eIdx);
-                if (keys.containsKey(key)) {
-                    res.append(keys.get(key));
-                } else {
-                    res.append('?');
-                }
+                int endIdx = i;
+
+                String key = s.substring(startIdx + 1, endIdx);
+                res.append(keys.getOrDefault(key, "?"));
             } else {
                 res.append(ch);
             }
